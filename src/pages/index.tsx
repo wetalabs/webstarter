ee9f876f02a0030a916589339f91f1aea7481871
曹操 <xuzhao.sgn@gmail.com>
2022-11-29 22:17:45 +0800

    add Meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,20 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
-const Title = "Head Title"
+import { Meta } from '../components/meta'
 
 export default function Home() {
 
   return (
     <>
-      <Head>
-        <meta
-          key="twitter:title"
-          name="twitter:title"
-          content="Tailwind CSS - Rapidly build modern websites without ever leaving your HTML."
-        />
-        <meta
-          key="og:title"
-          property="og:title"
-          content="Tailwind CSS - Rapidly build modern websites without ever leaving your HTML."
-        />
-        <title>{Title}</title>
-      </Head>
+      <Meta
+        title=''
+        description=''
+        image=''
+      />
       <div className="text-5xl font-bold underline font-sans">
         Hello World
       </div>
     </>
   )
 }
+
+
