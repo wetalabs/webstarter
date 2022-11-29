ff7a36db833f7cd84e36a0e7d2ba3f723060ee1f
曹操 <xuzhao.sgn@gmail.com>
2022-11-29 16:00:35 +0800

    fix issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+const Title = "Head Title"
+
 export default function Home() {
+
   return (
-    <div className="text-5xl font-bold underline font-sans">
-      Hello World
-    </div>
+    <>
+      <Head>
+        <meta
+          key="twitter:title"
+          name="twitter:title"
+          content="Tailwind CSS - Rapidly build modern websites without ever leaving your HTML."
+        />
+        <meta
+          key="og:title"
+          property="og:title"
+          content="Tailwind CSS - Rapidly build modern websites without ever leaving your HTML."
+        />
+        <title>{Title}</title>
+      </Head>
+      <div className="text-5xl font-bold underline font-sans">
+        Hello World
+      </div>
+    </>
   )
 }
