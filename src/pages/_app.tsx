4c6249e0255cd176fd24b0c370ae86f2601645a6
曹操 <xuzhao.sgn@gmail.com>
2023-03-15 17:19:26 +0800

    add captcha, api, toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
 import '../styles/fonts.css'
 import type { AppProps } from 'next/app'
+import NProgress from 'nprogress';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeStart', () =>  NProgress.start());
+    router.events.on('routeChangeComplete', () =>  NProgress.done());
+    router.events.on('routeChangeError', () =>  NProgress.done());
+  }, []);
 
   return <Component {...pageProps} />
   
